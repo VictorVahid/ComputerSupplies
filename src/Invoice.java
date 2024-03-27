10c138387b5f845db4904e1d00441d30eaea01e1
Victor Vahid <victorvahid76@example.com>
2024-03-27 02:04:52 -0400

    Initial commit

--- src/Invoice.java
+++ src/Invoice.java
@@ -4,21 +4,6 @@ public class Invoice {
 	public int amount;
 	public double uniteprice;
 
-	public Invoice(int identification, String description, int amount, double uniteprice) {
-		this.identification = identification;
-		System.out.println(this.identification);
-		this.description = description;
-		System.out.println(this.description);
-		this.amount = (amount > 0) ? amount : 0;
-		System.out.println(this.amount);
-		this.uniteprice = uniteprice > 0 ? uniteprice : 0.0;
-		System.out.println(this.uniteprice);
-	}
-
-	public Invoice() {
-
-	}
-
 	public String toString() {
 		return "Invoice ID: " + this.identification + "\nDescription: " + this.description +
 						"\nAmout: " + this.amount + "\nUnite Price: " + this.uniteprice;
